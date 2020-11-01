8e8ec2080f4eddc9ecf52a86b1c793c75d5a6de7
Eduardo <eduardoleonardo1@hotmail.com>
2020-11-01 11:32:30 -0300

    commit inicial

---
+++ src/Components/Avatar/Avatar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Avatar.css'
+
+const Avatar = () => {
+    return (
+        <div>
+            aqui é o avatar 
+        </div>
+    )
+}
+
+export default Avatar
