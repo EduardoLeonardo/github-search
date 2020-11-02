493642f5b0e41d9d280ad276a0fdb4010e40f299
Eduardo <eduardoleonardo1@hotmail.com>
2020-11-02 12:44:39 -0300

    perfil link

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Avatar from '../avatar/Avatar'
-import './Card.css'
+import React from "react";
+import Avatar from "../avatar/Avatar";
+import "./Card.css";
 
 const Card = ({ avatar_url: avatar, login: name, html_url: perfil }) => {
-    return (
-        <div className="card">
-            <Avatar src={avatar} width={100} style={{ borderRadius: 10 }} />
-            <h4>{name}</h4>
-        </div>
-    )
-}
+  return (
+    <div className="card">
+      <a href={perfil} target="_blank">
+        <Avatar src={avatar} width={100} style={{ borderRadius: 10 }} tar />
+      </a>
+      <h4>{name}</h4>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
